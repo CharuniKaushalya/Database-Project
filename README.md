e8dff064faf029fb305cf7d21e2ca01f36e48773
KanchanaMalsha <malshathilini10.13@cse.mrt.ac.lk>
2015-12-30 14:05:05 -0600

    add children of studying at present and past pupils

--- README.md
+++
@@ -1,4 +0,0 @@
-DB_Project
-==========
-
-A Symfony project created on December 13, 2015, 8:43 am.
--- src/ApplicantBundle/ApplicantBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ApplicantBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ApplicantBundle extends Bundle
-{
-}
